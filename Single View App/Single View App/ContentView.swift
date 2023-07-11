a68a0c7c235a69abd5b4e0802fb7f7304c3af1ec
SenjorJun <Solovyov.sergey98@yandex.ru>
2023-07-11 15:39:44 +0300

    add Single View App file

---
+++ Single View App/Single View App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Single View App
+//
+//  Created by Сергей Соловьёв on 11.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Single View App/Single View App/Single_View_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Single_View_AppApp.swift
+//  Single View App
+//
+//  Created by Сергей Соловьёв on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Single_View_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
